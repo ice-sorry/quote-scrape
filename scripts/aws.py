0eba17cd092f145a1126c4d65d90980c849ed16c
WvrLight <sSLight22@gmail.com>
2026-04-16 20:01:35 +0800

    allow for using dev key in local environment only

--- scripts/aws.py
+++ scripts/aws.py
@@ -11,14 +11,19 @@ load_dotenv(BASE_PATH / '.env')
 def __require_env(key):
     value = os.getenv(key)
     if value is None:
-        raise ValueError(f"Environment variable '{key}' is required but not set.")
+        logger.debug(f"Environment variable '{key}' is required but not set.")
+        
     return value
 
 def upload_quotes():
     logger.info('Starting upload of quotes to S3 bucket...')
+    
+    ACCESS_KEY = __require_env("ACCESS_KEY")
+    ACCESS_SECRET = __require_env("ACCESS_SECRET")
+    
     s3 = boto3.client('s3',
-                    # aws_access_key_id=__require_env("ACCESS_KEY"),
-                    # aws_secret_access_key=__require_env("ACCESS_SECRET")
+                    aws_access_key_id=ACCESS_KEY,           # Access credentials will only be used in local environment
+                    aws_secret_access_key=ACCESS_SECRET
                 )
 
     BUCKET_NAME = __require_env("BUCKET_NAME")
